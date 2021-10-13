ce167234540336fcfd808f97e6e2707ca6f4a191
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-13 18:14:42 +0300

    removed import, added src

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import * as defaultLogo from './images/logo.svg';
-import * as mutedLogo from './images/logo-muted.svg';
 
 const Logo = (prop) => {
     let src = '';
 
     if (prop.type === 'default' || prop.type === null || prop.type === undefined) {
-        src = defaultLogo.default.src;
+        src = './images/logo.svg';
     } else if(prop.type === 'muted') {
-        src = mutedLogo.default.src;
+        src = './images/logo-muted.svg';
     };
 
     return (

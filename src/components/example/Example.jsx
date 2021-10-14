5d579b259c03cb7e6b6def9c0789da2bf0071921
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-14 23:28:23 +0300

    added Header  component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,18 +3,18 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
-import Logo from "../logo/Logo";
+import Header from "../header/Header";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
+
         <Paper className={classNames(styles.paper)}>
-        
-        <Logo />
-        <Logo type={'muted'}/>
+          <Header />
+
           <Link href="/about" color="secondary">
-            
+
           </Link>
         </Paper>
       </Container>

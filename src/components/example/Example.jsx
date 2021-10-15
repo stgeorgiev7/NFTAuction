72b326e43ccac6b2587d7f2154e320be78d33048
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-15 19:19:14 +0300

    fixed grit background

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,12 +3,12 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
-import Header from "../header/Header";
+import Head from "../header/Header";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
-        <Header />
+      <Head/>
 
       <Container className={classNames(styles.container)} maxWidth="xl">
 

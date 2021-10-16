1a70c5baf6cf166a5269396395412dabbc7b5d92
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-16 14:11:49 +0300

    created Avatar

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,16 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Avatar as AvatarDiv } from '@mui/material';
+import { Badge } from '@mui/material';
+import defaultAvatar from './image/avatar.png';
+import badgeImg from './image/verified.svg';
 
-export default function Avatar({url, size, verified}) {
-    
-    const [image, setImage] = useState('./image/avatar.png');
+export default function Avatar({ url, size, verified }) {
 
-    console.log(url);
-    console.log(size);
-    console.log(verified);
+    const [avatarPic, setAvatar] = useState(url ? url : defaultAvatar.src);
+    const [avatarSize, setSize] = useState(size ? size : { width: 90, height: 90 });
+    const [verifiedAvatar, setVerified] = useState(verified? badgeImg.src:'');
 
-    
-    return(
-        <AvatarDiv alt='./image/avatar.png' src={image} >S</AvatarDiv>
+    return (
+        <Badge
+        overlap='circular' 
+        anchorOrigin={{vertical:'bottom', horizontal:'right'}}
+        badgeContent={<img src={verifiedAvatar}></img>}>
+            <AvatarDiv
+                src={avatarPic}
+                alt='Avatar'
+                sx={avatarSize}>
+
+            </AvatarDiv>
+        </Badge>
+
     );
 };
